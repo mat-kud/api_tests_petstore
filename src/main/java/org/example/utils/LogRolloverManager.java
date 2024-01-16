00a40e16bc97bb350f9d0eb18e9f9d83a893e735
Matt <henrykus888@gmail.com>
2024-01-16 20:37:47 +0100

    Added exception handlers for LogRolloverManager

--- src/main/java/org/example/utils/LogRolloverManager.java
+++ src/main/java/org/example/utils/LogRolloverManager.java
@@ -1,8 +1,8 @@
 package org.example.utils;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.nio.file.attribute.FileTime;
@@ -15,31 +15,57 @@ import static org.example.utils.AllureTestListener.LOG_PATH;
 public class LogRolloverManager {
     private static LogFilesRemover logFilesRemover = new LogFilesRemover();
 
-    public static void executeLogRollover(){
+    public static void executeLogRollover() {
         boolean isRollOn = Boolean.parseBoolean(System.getProperty("ROLL_ON"));
+        long rolloverSize;
+        long rolloverFrequency;
+
         if(!isRollOn){
             return;
         }
-        long rolloverFrequency = Long.parseLong(System.getProperty("ROLL_FREQ"));
-        if(isRolloverTriggered(rolloverFrequency)){
-            manageLogRollover(Path.of(LOG_PATH).toFile());
+
+        try {
+            rolloverSize = Long
+                    .parseLong(System.getProperty("ROLL_SIZE"))
+                    * 1000000;
+        } catch (NumberFormatException e) {
+            System.err.println("Value of ROLL_SIZE property cannot be parsed to long");
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            rolloverFrequency = Long.parseLong(System.getProperty("ROLL_FREQ"));
+        } catch (NumberFormatException e) {
+            System.err.println("Value of ROLL_FREQ property cannot be parsed to long");
+            e.printStackTrace();
+            return;
+        }
+
+        try {
+            if (isRolloverTriggered(rolloverFrequency)) {
+                manageLogRollover(Path.of(LOG_PATH).toFile(), rolloverSize);
+            }
+        } catch (InvalidPathException e) {
+            System.err.println("Provided path of logs directory cannot be converted to a Path");
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
-    private static long manageLogRollover(File directory) {
+    private static long manageLogRollover(File directory, long rolloverSize) {
         long length = 0;
-        long rolloverSize = Long
-                .parseLong(System.getProperty("ROLL_SIZE"))
-                *1000000;
+
         File[] files = directory.listFiles();
         if (files != null) {
             for (File file : files) {
                 if (file.isFile()) {
                     length += file.length();
                 } else {
-                    length += manageLogRollover(file);
+                    length += manageLogRollover(file, rolloverSize);
                 }
-                if (length > rolloverSize){
+                if (length > rolloverSize) {
                     logFilesRemover.deleteDirectory(Path.of(LOG_PATH).toFile());
                     return -1;
                 }
@@ -48,13 +74,13 @@ public class LogRolloverManager {
         return length;
     }
 
-    public static boolean isRolloverTriggered(long rolloverFrequency){
+    public static boolean isRolloverTriggered(long rolloverFrequency) {
         FileTime creationTime = null;
         try {
             creationTime = Files
                     .readAttributes(Path.of(LOG_PATH), BasicFileAttributes.class)
                     .creationTime();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
